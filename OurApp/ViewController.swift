8bd05b32d236ea47636f19b783f112eafafe396f
Sameer <sameer@miletap.com>
2020-03-16 17:48:52 +0500

    print

--- OurApp/ViewController.swift
+++ OurApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Gellow")
         // Do any additional setup after loading the view.
     }
 
