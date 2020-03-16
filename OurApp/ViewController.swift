ddb478ca3f846b2612255b9bdf94ae25e4c4a30e
Sameer <sameer@miletap.com>
2020-03-16 17:17:34 +0500

    Initial Commit

---
+++ OurApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OurApp
+//
+//  Created by Shuja on 16/03/2020.
+//  Copyright © 2020 SamLoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
